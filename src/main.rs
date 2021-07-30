463688197773b65bb09803df916b3e6a1bcc06f4
wandyezj <wandyezj@gmail.com>
2021-07-30 12:53:15 -0700

    basic lint

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ mod test;
 use regex::Regex;
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MetadataHeader {
+    present: bool,
+}
 
 #[derive(Serialize, Deserialize)]
 struct MetadataField {
@@ -19,11 +26,6 @@ struct MetadataField {
 struct MetadataRule {
     name: Option<String>,
     regex: Option<String>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct MetadataHeader {
-    present: bool,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -41,7 +43,7 @@ fn get_metadata(metadata_file_path: String) -> Metadata {
 
     // Do any checks on the metadata
     // Report any invlid regex
-    // report any field.rule that do not have a corresponding entry
+    // report any field.rule that do not have a corresponding entry in rules or are not defined internally
 
     return metadata;
 }
@@ -87,15 +89,92 @@ fn scan_csv(metadata: Metadata, data_file_path: String) {
         let expression = value.regex.as_ref();
         if expression.is_some() {
             let expression_string = expression.unwrap().as_str();
-            // will want error checking to know that all regex are valid, this should be done when the metadata is fetched
+            // TODO: check will want error checking to know that all regex are valid, this should be done when the metadata is fetched
             let re = Regex::new(expression_string).unwrap();
             rules.insert(key.to_string(), re);
         }
     }
 
     // Order rules for each value based on entry
+    let mut fields_regex: Vec<Option<&Regex>> = Vec::new();
+    let mut fields_name: Vec<&String> = Vec::new();
+    let name_default = "".to_string();
+    let mut fields_rule: Vec<&String> = Vec::new();
+
+    for field in metadata.fields.iter() {
+        let rule = field.rule.as_ref();
+
+        let field_name = field.name.as_ref().unwrap_or(&name_default);
+
+        let mut rule_name = &name_default;
+        let mut field_regex: Option<&Regex> = None;
+
+        if rule.is_some() {
+            rule_name = rule.unwrap();
+
+            let re = rules.get(rule_name).unwrap();
+            field_regex = Some(re);
+        }
+
+        fields_rule.push(rule_name);
+        fields_name.push(field_name);
+        fields_regex.push(field_regex);
+    }
 
     // Scan through CSV
+
+    let expected_value_count = fields_regex.len();
+
+    // Open File
+    let f = File::open(data_file_path).unwrap();
+
+    // line
+    let buffer = BufReader::new(f);
+    let mut line_number = -1;
+    for line in buffer.lines() {
+        line_number += 1;
+
+        let data = line.unwrap();
+
+        // go through each value in each line
+        let values_iterator = data.split('\t');
+        let mut values: Vec<String> = Vec::new();
+        for value in values_iterator {
+            values.push(value.to_string());
+        }
+
+        // check for correct number of values
+        let values_count = values.len();
+        if values_count != expected_value_count {
+            // TODO: collect information for log
+            println!(
+                "error: line [{}] has incorrect number of values [{}], expected [{}]",
+                line_number, values_count, expected_value_count
+            );
+            continue;
+        }
+
+        for field_number in 0..values_count {
+            let value = values.get(field_number).unwrap();
+            let re = fields_regex.get(field_number).unwrap();
+            let mut is_match = true;
+            if re.is_some() {
+                is_match = re.unwrap().is_match(value);
+            }
+
+            if !is_match {
+                //TODO: collect information for log
+                println!(
+                    "error: line [{}] field [{}] [{}] value [{}] does not match field rule [{}]",
+                    line_number + 1, /* line numbers start at 1*/
+                    field_number,
+                    fields_name.get(field_number).unwrap(),
+                    value,
+                    fields_rule.get(field_number).unwrap()
+                );
+            }
+        }
+    }
 
     // line
 
